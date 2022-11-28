5764b3c7d935b00beca9de8ee181ddd403bb16df
Starinin Andrey <starininandrey@gmail.com>
2022-11-28 18:35:30 +0300

    Add Article.cs

---
+++ StaticBlogGenerator.Lib/DAL/Article.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace StaticBlogGenerator.Lib.DAL;
+
+[Table("table_articles")]
+public class Article
+{
+    [DatabaseGenerated(DatabaseGeneratedOption.None)]
+    [Column("id")]
+    public Guid Guid { get; set; }
+    
+    [Column("title")]
+    [Required]
+    public string Title { get; set; }
+    
+    [Column("content")]
+    [Required]
+    public string Content { get; set; }
+    
+    [Column("creation_date")]
+    [Required]
+    public DateTime CreationDate { get; set; }
+    
+    [Column("last_modification_date")]
+    [Required]
+    public DateTime LastModificationDate { get; set; }
+}
