b5e8f9913b0affabd905ab014a92e6518c44f439
Starinin Andrey <starininandrey@gmail.com>
2022-11-29 16:23:16 +0300

    Add ArticleDbContext

---
+++ StaticBlogGenerator.Lib/DAL/ArticlesDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace StaticBlogGenerator.Lib.DAL;
+
+public sealed class ArticlesDbContext : DbContext
+{
+    public DbSet<Article> Articles { get; set; }
+
+    private readonly string _connectionString;
+
+    public ArticlesDbContext(string connectionString = "Data Source=articles.db;")
+    {
+        _connectionString = connectionString;
+        Database.EnsureCreated();
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlite(_connectionString);
+    }
+}
