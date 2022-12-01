53b6fb60f7d08406322e3577cb4776276e82a6c3
Starinin Andrey <starininandrey@gmail.com>
2022-12-01 16:56:35 +0300

    Update Lib (add IContext)

---
+++ StaticBlogGenerator.Lib/IContext.cs
@@ -0,0 +1,10 @@
+﻿namespace StaticBlogGenerator.Lib;
+
+public interface IContext<T>
+{
+    public void Insert(T obj);
+    public void Update(T obj);
+    public void Delete(T obj);
+    public T GetSingle(Guid id);
+    public IEnumerable<T> GetAll();
+}
