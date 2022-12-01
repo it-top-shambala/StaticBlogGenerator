ffd8749317159e1db7f8d38617706daedd491419
Starinin Andrey <starininandrey@gmail.com>
2022-12-01 16:57:00 +0300

    Update lib (implements IContext)

---
+++ StaticBlogGenerator.Lib/BL/ArticlesContext.cs
@@ -0,0 +1,38 @@
+﻿using StaticBlogGenerator.Lib.DAL;
+
+namespace StaticBlogGenerator.Lib.BL;
+
+public class ArticlesContext : IContext<Article>
+{
+    private IContext<Article> _context;
+
+    public ArticlesContext(IContext<Article> context)
+    {
+        _context = context;
+    }
+
+    public void Insert(Article obj)
+    {
+        _context.Insert(obj);
+    }
+
+    public void Update(Article obj)
+    {
+        _context.Update(obj);
+    }
+
+    public void Delete(Article obj)
+    {
+        _context.Delete(obj);
+    }
+
+    public Article GetSingle(Guid id)
+    {
+        return _context.GetSingle(id);
+    }
+
+    public IEnumerable<Article> GetAll()
+    {
+        return _context.GetAll();
+    }
+}
--- StaticBlogGenerator.Lib/DAL/ArticlesDbContext.cs
+++ StaticBlogGenerator.Lib/DAL/ArticlesDbContext.cs
@@ -2,7 +2,7 @@
 
 namespace StaticBlogGenerator.Lib.DAL;
 
-public sealed class ArticlesDbContext : DbContext
+public sealed class ArticlesDbContext : DbContext, IContext<Article>
 {
     public DbSet<Article> Articles { get; set; }
 
@@ -17,5 +17,38 @@ public sealed class ArticlesDbContext : DbContext
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
         optionsBuilder.UseSqlite(_connectionString);
+    }
+
+    public void Insert(Article obj)
+    {
+        obj.CreationDate = DateTime.Now;
+        obj.LastModificationDate = obj.CreationDate;
+        Articles.Add(obj);
+        this.SaveChanges();
+    }
+
+    public void Update(Article obj)
+    {
+        var article = Articles.First(a => a.Guid == obj.Guid);
+        article.Title = obj.Title;
+        article.Content = obj.Content;
+        article.LastModificationDate = DateTime.Now;
+        this.SaveChanges();
+    }
+
+    public void Delete(Article obj)
+    {
+        Articles.Remove(obj);
+        this.SaveChanges();
+    }
+
+    public Article GetSingle(Guid id)
+    {
+        return Articles.First(a => a.Guid == id);
+    }
+
+    public IEnumerable<Article> GetAll()
+    {
+        return Articles;
     }
 }
