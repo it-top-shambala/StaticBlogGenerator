4ad183bf6111dfdc63ead7364e6bb75ed961faf3
Starinin Andrey <starininandrey@gmail.com>
2022-12-06 18:17:58 +0300

    Add key

--- StaticBlogGenerator.Lib/DAL/Article.cs
+++ StaticBlogGenerator.Lib/DAL/Article.cs
@@ -8,13 +8,20 @@ public class Article
 {
     [DatabaseGenerated(DatabaseGeneratedOption.None)]
     [Column("id")]
+    [Key]
     public Guid Guid { get; set; }
 
-    [Column("title")] [Required] public string Title { get; set; }
+    [Column("title")]
+    [Required]
+    public string Title { get; set; }
 
-    [Column("content")] [Required] public string Content { get; set; }
+    [Column("content")]
+    [Required]
+    public string Content { get; set; }
 
-    [Column("creation_date")] [Required] public DateTime CreationDate { get; set; }
+    [Column("creation_date")]
+    [Required]
+    public DateTime CreationDate { get; set; }
 
     [Column("last_modification_date")]
     [Required]
