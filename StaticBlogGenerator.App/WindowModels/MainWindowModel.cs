239d0a8bf90dcf961d04112a7345e2fa2020e90a
Starinin Andrey <starininandrey@gmail.com>
2022-12-06 18:18:14 +0300

    Add App proj

---
+++ StaticBlogGenerator.App/WindowModels/MainWindowModel.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+using StaticBlogGenerator.Lib.BL;
+using StaticBlogGenerator.Lib.DAL;
+
+namespace StaticBlogGenerator.App.WindowModels;
+
+public class MainWindowModel
+{
+    public ObservableCollection<Article> Articles { get; set; }
+
+    private readonly ArticlesContext _articlesContext;
+
+    public MainWindowModel()
+    {
+        _articlesContext = new ArticlesContext(new ArticlesDbContext());
+        Articles = new ObservableCollection<Article>();
+        InitArticles();
+    }
+
+    private async Task InitArticles()
+    {
+        await foreach (var article in _articlesContext.GetAllAsyncEnumerable())
+        {
+            Articles.Add(article);
+        }
+    }
+}
---
+++ StaticBlogGenerator.App/Windows/MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace StaticBlogGenerator.App.Windows
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
