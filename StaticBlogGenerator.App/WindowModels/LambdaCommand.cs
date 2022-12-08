0864fb1682ec3d886b617f2506b3fb9126b700cc
Starinin Andrey <starininandrey@gmail.com>
2022-12-08 18:41:20 +0300

    Add BaseNotification & LambdaCommand

---
+++ StaticBlogGenerator.App/WindowModels/LambdaCommand.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace StaticBlogGenerator.App.WindowModels;
+
+public class LambdaCommand : ICommand
+{
+    public LambdaCommand(Action<object?> execute, Predicate<object?>? canExecute = null)
+    {
+        _execute = execute;
+        _canExecute = canExecute ?? (_ => true);
+    }
+
+    private readonly Predicate<object?> _canExecute;
+    private readonly Action<object?> _execute;
+
+    public bool CanExecute(object? parameter)
+    {
+        return _canExecute.Invoke(parameter);
+    }
+
+    public void Execute(object? parameter)
+    {
+        _execute.Invoke(parameter);
+    }
+
+    public event EventHandler? CanExecuteChanged;
+
+    public void OnCanExecuteChanged()
+    {
+        CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+    }
+}
--- StaticBlogGenerator.App/WindowModels/MainWindowModel.cs
+++ StaticBlogGenerator.App/WindowModels/MainWindowModel.cs
@@ -1,28 +1,56 @@
-﻿using System.Collections.ObjectModel;
+﻿using System;
+using System.Collections.ObjectModel;
 using System.Threading.Tasks;
 using StaticBlogGenerator.Lib.BL;
 using StaticBlogGenerator.Lib.DAL;
 
 namespace StaticBlogGenerator.App.WindowModels;
 
-public class MainWindowModel
+public class MainWindowModel : BaseNotification
 {
+    #region Observable properties
+
+    private Article _article;
+
+    public Article SelectedArticle
+    {
+        get => _article;
+        set => SetField(ref _article, value);
+    }
+
     public ObservableCollection<Article> Articles { get; set; }
+
+    #endregion
+
+    #region Commands
+
+    public LambdaCommand CommandConnect { get; set; }
+
+    #endregion
+
 
     private readonly ArticlesContext _articlesContext;
 
     public MainWindowModel()
     {
+        _article = new Article();
         _articlesContext = new ArticlesContext(new ArticlesDbContext());
         Articles = new ObservableCollection<Article>();
-        InitArticles();
+
+        CommandConnect = new LambdaCommand(
+            execute: async _ => await InitArticles(),
+            canExecute: _ => Articles.Count == 0
+            );
     }
 
     private async Task InitArticles()
     {
+        Articles.Clear();
+
         await foreach (var article in _articlesContext.GetAllAsync())
         {
             Articles.Add(article);
         }
+        CommandConnect.OnCanExecuteChanged();
     }
 }
